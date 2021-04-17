c5e69b24662b87ec4049cae2991c02f70ae53165
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-04-17 11:42:37 -0500

    Updated functionality

--- password-chaII/Develop/script.js
+++ password-chaII/Develop/script.js
@@ -3,30 +3,28 @@
 // Create an array of the four choices (Lower-case, Upper-case, Numbers, Symbols)
 
 
+var numberChars = "0123456789";
+var upperChars = "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var symbolChars = "!@#$%^&*()_+=-{}|[]:'?><,.";
+var allChars = numberChars + upperChars + lowerChars + symbolChars;
+// var randPasswordArray = Array(passwordLength);
 
+// randPasswordArray [0] = numberChars;
+// randPasswordArray [1] = upperChars;
+// randPasswordArray [2] = lowerChars;
+// randPasswordArray [3] = symbolChars;
 
+// randPasswordArray = randPasswordArray.fill(allChars, 4);
+// return shuffleArray(randPasswordArray.map(function(x); 
 
-  
-  var numberChars = "0123456789";
-  var upperChars = "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
-  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-  var symbolChars = "!@#$%^&*()_+=-{}|[]:'?><,.";
-  var allChars = numberChar + upperChars + lowerChars + symbolChars;
-  var randPasswordArray = Array(passwordLength);
+// { return x[Math.floor(Math.random()* x.length)] })).join('')
 
-  randPasswordArray [0] = numberChars;
-  randPasswordArray [1] = upperChars;
-  randPasswordArray [2] = lowerChars;
-  randPasswordArray [3] = symbolChars;
+// }
 
-  randPasswordArray = randPasswordArray.fill(allChars, 4);
-  return shuffleArray(randPasswordArray.map(function(x); { return x[Math.floor(Math.random()* x.length)] })).join('');
-
-  }
-
-  function shuffleAray(array {
-    for (var i = array.length - 1; i--)
-  })
+// function shuffleArray(array {
+//   for (var i = array.length - 1; i--)
+// })
 
 
 
@@ -43,6 +41,48 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  var password = ""
+  var passwordSize = prompt("What is the size of your password? (It must be more than 8 characters and less than 128 characters)")
+  passwordSize = parseInt(passwordSize)
 
+  if (passwordSize >= 8 && passwordSize <= 128) {
+
+    var isLowercase = confirm("Do you want lowercase on your password?")
+    var isUppercase = confirm("Do you want lowercase on your password?")
+    var isNumeric = confirm("Do you want numbers in your password?")
+    var isSpecialCharacters = confirm("Do you want special characters on your password?")
+
+    for (let i = 0; password.length   < passwordSize; i++) {
+      if (isLowercase  && password.length   < passwordSize) {
+        var index = Math.floor(Math.random() * lowerChars.length)
+        password = password + lowerChars.charAt(index) //"" +"f" result=> "f"
+      }
+  
+      if (isUppercase && password.length   < passwordSize) {
+        var index = Math.floor(Math.random() * upperChars.length)
+        password = password + upperChars.charAt(index) //"" +"f" result=> "f"
+      }
+      if (isNumeric && password.length   < passwordSize) {
+        var index = Math.floor(Math.random() * numberChars.length)
+        password = password + numberChars.charAt(index) //"" +"f" result=> "f"
+      }
+      if (isSpecialCharacters && password.length   < passwordSize){
+        var index = Math.floor(Math.random()*symbolChars.length)
+        password = password + symbolChars.charAt(index) //"" +"f" result=> "f"
+     }
+      
+    }
+
+ 
+
+    return password
+
+
+  }
+  else {
+    alert("Invalid entry")
+  }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
